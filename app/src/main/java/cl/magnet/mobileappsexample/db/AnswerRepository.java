3b8eecb77a5eaad7a2916ebc42b56e551eef9e28
nhurtado <nhurtado@miuandes.cl>
2018-05-29 17:11:28 -0400

    Parte 3.

---
+++ app/src/main/java/cl/magnet/mobileappsexample/db/AnswerRepository.java
@@ -0,0 +1,43 @@
+package cl.magnet.mobileappsexample.db;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class AnswerRepository {
+    private AnswerDao mAnswerDao;
+    private LiveData<List<Answer>> mAllAnswers;
+
+    AnswerRepository(Application application) {
+        AppDatabase db = AppDatabase.getDatabase(application);
+        mAnswerDao = db.answerDao();
+        mAllAnswers = mAnswerDao.getAllAnswers();
+    }
+
+    LiveData<List<Answer>> getAllAnswers() {
+        return mAllAnswers;
+    }
+
+    public void insert (List<Answer> answers) {
+        Answer[] answersArray = new Answer[answers.size()];
+        answersArray = answers.toArray(answersArray);
+        new AnswerRepository.insertAsyncTask(mAnswerDao).execute(answersArray);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Answer, Void, Void> {
+
+        private AnswerDao mAsyncTaskDao;
+
+        insertAsyncTask(AnswerDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Answer... params) {
+            mAsyncTaskDao.insertAll(params);
+            return null;
+        }
+    }
+}
